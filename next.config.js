732c303e4d7fd5283bcb46c27c15298e19d30a8d
kmnky <cubethe27@gmail.com>
2024-07-15 10:15:20 +0900

    リダイレクトで改善するかテスト

--- next.config.js
+++ next.config.js
@@ -8,3 +8,15 @@ const nextConfig = {
 };
 
 module.exports = withContentlayer(nextConfig);
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/tags/reading_diary',
+        destination: '/tags/%E8%AA%AD%E6%9B%B8%E6%97%A5%E8%A8%98',
+        permanent: true
+      }
+    ]
+  }
+}
